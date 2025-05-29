0b719722e712b0b42df02e31ed26dd311e22c958
yongyutjantaboot <yuth_style88@hotmail.com>
2025-05-29 17:31:22 +0700

    update initDBforAdblockUpdater.js

--- scripts/initDBforAdblockUpdater.js
+++ scripts/initDBforAdblockUpdater.js
@@ -1,36 +1,37 @@
 import util from '../lib/util.js'
 import { getListCatalog,resourcesComponentId,regionalCatalogComponentId } from '../lib/adBlockRustUtils.js'
 import commander from 'commander'
-const initDB = (commander) => {
-  util.createTableIfNotExists(commander.endpoint, commander.region).then(async () => {
-    let catalog = await getListCatalog()
-    const regionalItem = {
-      "title": "iBrowe Ad Block Updater (Regional Catalog)",
-      "list_text_component": {
-        "component_id": regionalCatalogComponentId,
-      }
+const initDB = async (commander) => {
+  await util.createTableIfNotExists(commander.endpoint, commander.region)
+  let catalog = await getListCatalog()
+  const regionalItem = {
+    title: "iBrowe Ad Block Updater (Regional Catalog)",
+    list_text_component: {
+      component_id: regionalCatalogComponentId,
     }
-    const resourceItem = {
-      "title": "iBrowe Ad Block Updater (Resources)",
-      "list_text_component": {
-        "component_id": resourcesComponentId,
-      }
+  }
+  const resourceItem = {
+    title: "iBrowe Ad Block Updater (Resources)",
+    list_text_component: {
+      component_id: resourcesComponentId,
     }
-    catalog.push(regionalItem)
-    catalog.push(resourceItem)
-    catalog.forEach(entry => {
+  }
+  catalog.push(regionalItem)
+  catalog.push(resourceItem)
+  await Promise.all(
+    catalog.map(entry =>
       util.insertExtension(
         commander.endpoint,
         commander.region,
-        entry.list_text_component.component_id,           // version
-        `iBrowe Ad Block Updater (${entry.title})`,              // title
+        entry.list_text_component.component_id,
+        `iBrowe Ad Block Updater (${entry.title})`
       )
-    })
-  })
+    )
+  )
 }
 
-const processJob = (commander) => {
-  initDB(commander)
+const processJob = async (commander) => {
+  await initDB(commander)
 }
 
 util.installErrorHandlers()
@@ -40,4 +41,4 @@ util.addCommonScriptOptions(
     .option('-l, --local-run', 'Runs updater job without connecting anywhere remotely'))
   .parse(process.argv)
 
-  processJob(commander)
+await processJob(commander)
